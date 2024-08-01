b19037e2072ed5c82bc4674fcaf35e2949076a12
e.dyakov <e3001dk@gmail.com>
2024-08-01 12:33:59 +0300

    добавляет business

--- js/swiper.js
+++ js/swiper.js
@@ -33,3 +33,33 @@ const swiper2 = new Swiper('.swiper2', {
   },
 })
 
+const swiper3 = new Swiper('.swiper3', {
+  direction: 'horizontal',
+  loop: true,
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1.2,
+    },
+    // 600: {
+    //   slidesPerView: 2.3,
+    // },
+    900: {
+      slidesPerView: 2.3,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
